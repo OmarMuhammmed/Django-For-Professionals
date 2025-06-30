48f8eb1023f168f1a4dc991cecd406c3c21b8ff3
OmarMuhammmed <amorey2006@gmail.com>
2025-06-30 15:59:43 +0300

    feat:build search functinailty

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
 )
+from django.db.models import Q
 
 from .models import Book
 
@@ -16,9 +17,21 @@ class BookListView(ListView, LoginRequiredMixin):
 class BookDetailView(LoginRequiredMixin,
                      PermissionRequiredMixin,
                      DetailView
-                    ):
+                     ):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'books_searched'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        qurey = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=qurey) | Q(author__icontains=qurey)
+        )
